d60b972deb030a5b62764354d40ae9df26c9902b
Aleksander Petuskey <17petuskey@berkeley.edu>
2022-01-09 18:32:24 -0800

    Updates

---
+++ precache-manifest.3bfac0e9aa8bb7ca7709bdefb5ef594f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4534fc0960601d973efe437bdb2b0728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077326da313a0e89a51c",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "4c8256031dea4138a31e",
+    "url": "/static/js/2.e2792814.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.e2792814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077326da313a0e89a51c",
+    "url": "/static/js/main.0465e67f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "263eff8e16ffe2ed04344e86c1595503",
+    "url": "/static/media/alekpetuskey.263eff8e.jpeg"
+  },
+  {
+    "revision": "a96a14059fd131277265270567920cac",
+    "url": "/static/media/resume.a96a1405.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3a66d691f0e984dcf9717c470173201b.js"
+  "/precache-manifest.3bfac0e9aa8bb7ca7709bdefb5ef594f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
